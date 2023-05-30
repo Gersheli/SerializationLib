b145c3ee1b14ec4096fcb2ac1e95375acd9e5775
Victor <siva500000217@mail.ru>
2023-05-30 12:50:45 +0300

    fixed json

--- src/main/java/JsonSerializer.java
+++ src/main/java/JsonSerializer.java
@@ -5,6 +5,7 @@ import java.io.*;
 public class JsonSerializer {
     private final ObjectOutputStream objectOutputStream;
     private final ObjectInputStream objectInputStream;
+    private Class aClass;
 
     public JsonSerializer(String source) throws IOException {
         FileOutputStream outputStream = new FileOutputStream(source);
@@ -17,12 +18,13 @@ public class JsonSerializer {
         Gson gson = new Gson();
         String json = gson.toJson(object);
         objectOutputStream.writeObject(json);
+        aClass = object.getClass();
     }
 
     public Serializable deserialize() throws IOException, ClassNotFoundException {
         Gson gson = new Gson();
         String json = (String)objectInputStream.readObject();
-        return gson.fromJson(json, MyClass.class);
+        return gson.fromJson(json, aClass.getClass());
     }
 
     public void closeStream() throws IOException {
